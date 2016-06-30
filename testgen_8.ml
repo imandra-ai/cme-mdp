ad710036b48c2c7751ea1ae9e8b44398852f9446
KKostya <kkanishev@gmail.com>
2016-06-30 23:44:24 +0200

    plugging Yojson with Kojson

--- testgen_8.ml
+++ testgen_8.ml
@@ -2,7 +2,8 @@
 
 :load_ocaml CME_printers.ml
 :load_ocaml CME_test_helper.ml
-:load_ocaml CME_to_json_simple.ml
+:load_ocaml kojson.ml
+:load_ocaml CME_json.ml
 :load_ocaml CME_test_printer_del.ml
 
 :adts
@@ -22,25 +23,29 @@
 
 let large_call (s, msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8) =
     let s' = { s with
-           books = { book_depth = 5; multi = {buys = []; sells = []};
-         implied = {buys = []; sells = []};
-        combined = {buys = []; sells = []};
-        b_status = Publishable;
-    };
-    channels = { 
-       s.channels with ref_a = { s.channels.ref_a with r_unproc_packets = [msg1; msg2; msg3] };
-                       ref_b = { s.channels.ref_b with r_unproc_packets = [msg4; msg5] };
-                       snap_a = { s.channels.snap_a with s_unproc_packets = [msg6; msg7; msg8] };
-                       snap_b = { s.channels.snap_b with s_unproc_packets = [] };
-    };
- } in
- let s' = one_step(s') in 
- let s' = one_step(s') in 
- let s' = one_step(s') in 
- let s' = one_step(s') in 
- let s' = one_step(s') in 
- let s' = one_step(s') in 
- let s' = one_step(s') in 
- one_step(s')
+        books = { 
+            book_depth = 5; multi = {buys = []; sells = []};
+            implied = {buys = []; sells = []};
+            combined = {buys = []; sells = []};
+            b_status = Publishable; 
+        };
+        channels = { s.channels with 
+            ref_a  = { s.channels.ref_a with r_unproc_packets = [msg1; msg2; msg3]; };
+            ref_b  = { s.channels.ref_b with r_unproc_packets = [msg4; msg5]        };
+            snap_a = { s.channels.snap_a with s_unproc_packets = [msg6; msg7; msg8] };
+            snap_b = { s.channels.snap_b with s_unproc_packets = [] };
+            last_seq_processed = 0;
+            last_snapshot = None;
+            cache = [];
+        };
+    } in
+    let s' = one_step(s') in 
+    let s' = one_step(s') in 
+    let s' = one_step(s') in 
+    let s' = one_step(s') in 
+    let s' = one_step(s') in 
+    let s' = one_step(s') in 
+    let s' = one_step(s') in 
+    one_step(s')
 ;;
 :testgen large_call with_printer cme_test_printer_8
