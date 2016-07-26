4995d517662d0fe21962e8760d1c197495c2e931
KKostya <kkanishev@gmail.com>
2016-07-26 23:36:39 +0200

    Updating CME_json to the new organization

--- Printers/CME_json.ml
+++ Printers/CME_json.ml
@@ -3,190 +3,175 @@
     Aesthetic Integration Ltd.
     Copyright 2016
     
-    CME_printers.ml
+    CME_json.ml
 
 *)
 
-(* @meta[imandra_ignore] on @end *)
-open CME;;
-(* @meta[imandra_ignore] off @end *)
+let intopt_to_json : ( int option -> Yojson.Basic.json ) = 
+    function Some x -> `Int x | None -> `Assoc []
+;;
 
-let packet_to_json (packet : packet) : Kojson.json  =
-    let open Kojson in
-    let convert_header h = Assoc [
-        ("SequenceNumber", Int h.ph_packet_seq_num);
-        ("SendingTime",    Int h.ph_sending_time)
-        ] in
-    let convert_inc  p = Assoc [
-        ( "SecurityID", Int p.rm_security_id );
-        ( "RepSeqNum",  Int p.rm_rep_seq_num );
-        ( "PriceLevel", Int p.rm_price_level );
-        ( "EntrySize",  Int p.rm_entry_size  );
-        ( "EntryPrice", Int p.rm_entry_px    );
-        ( "NumOrders",  Int p.rm_num_orders  );
-        ( "MessageType",   match p.rm_msg_type with
-            | V_MDUpdateAction_New         -> String "New"       
-            | V_MDUpdateAction_Change      -> String "Change"    
-            | V_MDUpdateAction_Delete      -> String "Delete"    
-            | V_MDUpdateAction_DeleteThru  -> String "DeleteThru"
-            | V_MDUpdateAction_DeleteFrom  -> String "DeleteFrom"
-            | V_MDUpdateAction_Overlay     -> String "Overlay"    
-            );
-        ( "EntryType", match p.rm_entry_type with
-            | V_MDEntryType_Bid                      -> String "Bid"                    
-            | V_MDEntryType_Offer                    -> String "Offer"                  
-            | V_MDEntryType_ImpliedBid               -> String "ImpliedBid"             
-            | V_MDEntryType_ImpliedOffer             -> String "ImpliedOffer"  
-            | V_MDEntryType_EmptyBook                -> String "EmptyBook"              
-         
-          (*
-            | V_MDEntryType_TradeSummary             -> String "TradeSummary"           
-            | V_MDEntryType_OpeningPrice             -> String "OpeningPrice"           
-            | V_MDEntryType_SettlementPrice          -> String "SettlementPrice"        
-            | V_MDEntryType_TradingSessionHighPrice  -> String "TradingSessionHighPrice"
-            | V_MDEntryType_TradingSessionLowPrice   -> String "TradingSessionLowPrice" 
-            | V_MDEntryType_SessionHighBid           -> String "SessionHighBid"         
-            | V_MDEntryType_SessionLowOffer          -> String "SessionLowOffer"        
-            | V_MDEntryType_TradeVolume              -> String "TradeVolume"            
-            | V_MDEntryType_OpenInterest             -> String "OpenInterest"           
-            | V_MDEntryType_FixingPrice              -> String "FixingPrice"            
-            | V_MDEntryType_ElectronicVolume         -> String "ElectronicVolume"       
-            | V_MDEntryType_ThresholdLimits          -> String "ThresholdLimits"   *) 
-            ) 
-        ] in
-    let convert_order_level = function
-        | NoLevel -> Assoc []
-        | Level o -> Assoc [
-            ("Side",      match o.side with OrdBuy -> String "BUY" | OrdSell -> String "SELL");
-            ("Quantity", Int o.qty   );
-            ("Price",    Int o.price  );
-            ("NumOrders", match o.num_orders with None -> Assoc [] | Some n -> Int n )
+let intopt_of_json : ( Yojson.Basic.json -> int option) = 
+    function `Int x -> Some x | _ -> None 
+;;
+
+(* NOTE: I'm using List.assoc here -- I'm not sure whether I should try Hashtbl. *)
+(* But worth trying and benchmarking. *)
+
+let msgtype_to_json, msgtype_of_json =
+    let open CME_Types in
+    let m2j : (msg_type * Yojson.Basic.json ) list = [
+        ( V_MDUpdateAction_New        , `String "New"        );
+        ( V_MDUpdateAction_Change     , `String "Change"     );
+        ( V_MDUpdateAction_Delete     , `String "Delete"     );
+        ( V_MDUpdateAction_DeleteThru , `String "DeleteThru" );
+        ( V_MDUpdateAction_DeleteFrom , `String "DeleteFrom" );
+        ( V_MDUpdateAction_Overlay    , `String "Overlay"    ) ] in
+    let j2m = List.map (fun (a,b) -> b,a ) m2j in
+    (fun x -> List.assoc x m2j), (fun x -> List.assoc x j2m  )
+;;
+
+
+let entrytype_to_json, entrytype_of_json = 
+    let open CME_Types in
+    let e2j : ( entry_type * Yojson.Basic.json ) list = [
+        ( V_MDEntryType_Bid                      , `String "Bid"          );
+        ( V_MDEntryType_Offer                    , `String "Offer"        );
+        ( V_MDEntryType_ImpliedBid               , `String "ImpliedBid"   );
+        ( V_MDEntryType_ImpliedOffer             , `String "ImpliedOffer" );
+        ( V_MDEntryType_EmptyBook                , `String "EmptyBook"    ) ] in
+    let j2e = List.map (fun (a,b) -> b,a ) e2j in
+    (fun x -> List.assoc x e2j), (fun x -> List.assoc x j2e )   
+;;
+
+
+let channel_to_json, channel_of_json = 
+    let open CME_Types in
+    let c2j : ( channel_type * Yojson.Basic.json ) list = [
+        ( Ch_Ref_A  , `String "RefreshA"  );
+        ( Ch_Ref_B  , `String "RefreshB"  );
+        ( Ch_Snap_A , `String "SnapshotA" );
+        ( Ch_Snap_B , `String "SnapshotB" ) ] in
+    let j2c = List.map (fun (a,b) -> b,a ) c2j in
+    (fun x -> List.assoc x c2j), (fun x -> List.assoc x j2c )   
+;;
+
+
+let book_to_json : ( CME_Types.book -> Yojson.Basic.json ) =
+    let open CME_Types in
+    let convert_order_level : (order_level -> Yojson.Basic.json) = function 
+        | None -> `Assoc []
+        | Some o -> `Assoc [
+            ("Side",      match o.side with OrdBuy -> `String "BUY" | OrdSell -> `String "SELL");
+            ("Quantity", `Int o.qty   );
+            ("Price",    `Int o.price  );
+            ("NumOrders", match o.num_orders with None -> `Assoc [] | Some n -> `Int n )
         ] in 
-    let convert_snap p = Assoc[
-            ("SecurityID", Int p.sm_security_id );
-            ("RepSeqNum",  Int p.sm_rep_seq_num );
-            ("LastMsgSeqNumProcessed",  Int p.sm_last_msg_seq_num_processed );
-            ("RealBid", convert_order_level p.sm_real_bid );
-            ("RealAsk", convert_order_level p.sm_real_ask );
-            ("ImplBid", convert_order_level p.sm_imp_bid  );
-            ("ImplAsk", convert_order_level p.sm_imp_ask  )
+    function { buys = buys; sells = sells } -> `Assoc [
+        ( "Buys" , `List ( List.map convert_order_level buys  ) ); 
+        ( "Sells", `List ( List.map convert_order_level sells ) )
+    ]
+;;
+    
+
+let book_of_json : ( Yojson.Basic.json -> CME_Types.book ) =
+    let open CME_Types in
+    let open Yojson.Basic.Util in
+    let read_order_level : ( Yojson.Basic.json -> order_level ) = function 
+        | `Assoc [] -> None
+        | j -> Some { 
+            qty        = j |> member "Quantity"  |> to_int;
+            price      = j |> member "Price"     |> to_int;
+            num_orders = j |> member "NumOrders" |> to_int_option;
+            side       = j |> member "Side"      |>  (function 
+                | `String "BUY"  -> OrdBuy 
+                | `String "SELL" -> OrdSell
+                | _ -> failwith "Side should be either BUY or SELL" )
+        } in 
+    function j -> { 
+        buys  = j |> member "Buys"  |> to_list |> List.map read_order_level; 
+        sells = j |> member "Sells" |> to_list |> List.map read_order_level
+    }
+;;    
+    
+
+let message_to_json (message : CME_Types.message) : Yojson.Basic.json  =
+    let open CME_Types in
+    let convert_inc_refresh : ref_message -> Yojson.Basic.json = 
+        fun p -> `Assoc [
+            ( "SequrityID", `Int p.rm_security_id );
+            ( "RepSeqNum",  `Int p.rm_rep_seq_num );
+            ( "PriceLevel", `Int p.rm_price_level );
+            ( "EntrySize",  `Int p.rm_entry_size  );
+            ( "EntryPrice", `Int p.rm_entry_px    );
+            ( "NumOrders",   intopt_to_json p.rm_num_orders     );
+            ( "MessageType", msgtype_to_json   p.rm_msg_type    );
+            ( "EntryType",   entrytype_to_json p.rm_entry_type  ) 
         ] in
-    match packet with
-        | SnapshotPacket   p -> Assoc [ ( "SnapshotPacket" ,   
-            Assoc [ ( "Header" , convert_header p.sp_header  );
-                     ( "Message", convert_snap p.sp_snap ) ] 
-            ) ]
-        | IncRefreshPacket p -> Assoc [ ( "IncRefreshPacket" , 
-            Assoc [ ( "Header" , convert_header p.rp_header  );
-                     ( "Message", convert_inc p.rp_msg ) ] 
-            ) ]
-        | NoPacket -> Assoc []
+   let convert_snap_message : snap_message -> Yojson.Basic.json =
+        fun p -> `Assoc [
+            ( "SecurityID", `Int p.sm_security_id );
+            ( "RepSeqNum",  `Int p.sm_rep_seq_num );
+            ( "LastMsgSeqNumProcessed",  `Int p.sm_snapshot.snap_last_msg_seq_num_processed );
+            ( "MultiBook"   , book_to_json p.sm_snapshot.snap_m_book );
+            ( "ImpliedBook" , book_to_json p.sm_snapshot.snap_i_book )
+        ] in
+    match message with
+        | RefreshMessage  m -> convert_inc_refresh  m 
+        | SnapshotMessage m -> convert_snap_message m
 ;;
         
-
-let packets_to_json (packets : packet list) : Kojson.json  =
-    let open Kojson in 
-    Assoc [( "packets", List ( List.map packet_to_json packets ))] ;;
-
-let packets_to_string (packets : packet list) : string  = 
-    packets |> packets_to_json |> Kojson.to_string;;
-
-(*    
-let packet_of_json ( json : Kojson.json) : packet   =
-    let open Kojson in
-    let fail msg = raise (Failure ("Malformed packet JSON: " ^ msg )) in
-    let getint = function Int n -> n | _ -> fail "integer is expected" in
-    let read_header = function 
-        | Assoc lst -> {
-                ph_packet_seq_num = lst |> List.assoc "SequenceNumber" |> getint;
-                ph_sending_time   = lst |> List.assoc "SendingTime"    |> getint 
-            } 
-        | _ -> fail "malformed header"
-        in
-    let read_inc = function
-        | Assoc lst -> {
-                rm_security_id = lst |> List.assoc "SequrityID" |> getint;
-                rm_rep_seq_num = lst |> List.assoc "RepSeqNum"  |> getint;
-                rm_price_level = lst |> List.assoc "PriceLevel" |> getint;
-                rm_entry_size  = lst |> List.assoc "EntrySize"  |> getint;
-                rm_entry_px    = lst |> List.assoc "EntryPrice" |> getint;
-                rm_num_orders  = lst |> List.assoc "NumOrders"  |> getint;
-                rm_msg_type    = lst 
-                    |> List.assoc "MessageType" 
-                    |> ( function 
-                          | String "New"        ->  V_MDUpdateAction_New         
-                          | String "Change"     ->  V_MDUpdateAction_Change      
-                          | String "Delete"     ->  V_MDUpdateAction_Delete      
-                          | String "DeleteThru" ->  V_MDUpdateAction_DeleteThru  
-                          | String "DeleteFrom" ->  V_MDUpdateAction_DeleteFrom  
-                          | String "Overlay"    ->  V_MDUpdateAction_Overlay     
-                          | _ -> fail "bad message type" );
-                rm_entry_type = lst
-                    |> List.assoc "EntryType"
-                    |> ( function 
-                         | String "Bid"                     -> V_MDEntryType_Bid                    
-                         | String "Offer"                   -> V_MDEntryType_Offer                  
-                         | String "ImpliedBid"              -> V_MDEntryType_ImpliedBid             
-                         | String "ImpliedOffer"            -> V_MDEntryType_ImpliedOffer
-                         | String "EmptyBook"               -> V_MDEntryType_EmptyBook              
-                      (*
-                         | String "TradeSummary"            -> V_MDEntryType_TradeSummary           
-                         | String "OpeningPrice"            -> V_MDEntryType_OpeningPrice           
-                         | String "SettlementPrice"         -> V_MDEntryType_SettlementPrice        
-                         | String "TradingSessionHighPrice" -> V_MDEntryType_TradingSessionHighPrice
-                         | String "TradingSessionLowPrice"  -> V_MDEntryType_TradingSessionLowPrice 
-                         | String "SessionHighBid"          -> V_MDEntryType_SessionHighBid         
-                         | String "SessionLowOffer"         -> V_MDEntryType_SessionLowOffer        
-                         | String "TradeVolume"             -> V_MDEntryType_TradeVolume            
-                         | String "OpenInterest"            -> V_MDEntryType_OpenInterest           
-                         | String "FixingPrice"             -> V_MDEntryType_FixingPrice            
-                         | String "EmptyBook"               -> V_MDEntryType_EmptyBook              
-                         | String "ElectronicVolume"        -> V_MDEntryType_ElectronicVolume       
-                         | String "ThresholdLimits"         -> V_MDEntryType_ThresholdLimits     *)   
-                         | _ -> fail "bad entry type")
+    
+let message_of_json ( json : Yojson.Basic.json) : CME_Types.message =
+    let open Yojson.Basic.Util in
+    let open CME_Types in
+    (* NOTE : Cutting ALL negative integers across the board *)
+    let to_int j = to_int j |> max 0 in 
+    let read_inc j = {
+            rm_security_id = j |> member "SequrityID"  |> to_int;
+            rm_rep_seq_num = j |> member "RepSeqNum"   |> to_int;
+            rm_price_level = j |> member "PriceLevel"  |> to_int |> min 255;  (* NOTE: fix for pricelevel above 255*)
+            rm_entry_size  = j |> member "EntrySize"   |> to_int;
+            rm_entry_px    = j |> member "EntryPrice"  |> to_int;
+            rm_num_orders  = j |> member "NumOrders"   |> intopt_of_json;
+            rm_msg_type    = j |> member "MessageType" |> msgtype_of_json;
+            rm_entry_type  = j |> member "EntryType"   |> entrytype_of_json
+        } in
+    let read_snap j = {
+            sm_security_id = j |> member "SecurityID" |> to_int ;
+            sm_rep_seq_num = j |> member "RepSeqNum"  |> to_int ;
+            sm_snapshot = {
+                snap_last_msg_seq_num_processed = j |> member "LastMsgSeqNumProcessed" |> to_int;
+                snap_m_book  = j |> member "MultiBook"   |> book_of_json;
+                snap_i_book  = j |> member "ImpliedBook" |> book_of_json
             }
-        | _ -> fail "malformed incref packet"
-        in
-    let read_order_level = function
-        | Assoc []  -> NoLevel  
-        | Assoc lst -> Level {
-                qty   = lst |> List.assoc "Quantity" |> getint;
-                price = lst |> List.assoc "Price"    |> getint;
-                side  = lst |> List.assoc "Side" 
-                                |> (function | String "BUY"  -> BUY 
-                                             | String "SELL" -> SELL
-                                             | _ -> fail "side should be either BUY or SELL");
-                num_orders = lst |> List.assoc "NumOrders" 
-                                     |> (function | Assoc [] -> None
-                                                  | Int n -> Some n
-                                                  | _ -> fail "Bad NUmOrders" )
-            }
-        | _ -> fail "bad order level"
-       in 
-    let read_snap  = function
-        | Assoc lst -> {
-                sm_security_id = lst |> List.assoc "SecurityID" |> getint ;
-                sm_rep_seq_num = lst |> List.assoc "RepSeqNum"  |> getint ;
-                sm_last_msg_seq_num_processed = lst |> List.assoc "LastMsgSeqNumProcessed" |> getint;
-                sm_real_bid    = lst |> List.assoc "RealBid" |> read_order_level;
-                sm_real_ask    = lst |> List.assoc "RealAsk" |> read_order_level;
-                sm_imp_bid     = lst |> List.assoc "ImplBid" |> read_order_level;
-                sm_imp_ask     = lst |> List.assoc "ImplAsk" |> read_order_level
-            }
-        | _ -> fail "malformed snap packet"
-        in
+        } in
     match json with
-        | Assoc [ ( "SnapshotPacket" ,   Assoc lst ) ] ->
-            let h, m = List.assoc "Header" lst, List.assoc "Message" lst in
-            SnapshotPacket   { sp_header = read_header h; sp_snap = read_snap m }
-        | Assoc [ ( "IncRefreshPacket" , Assoc lst ) ] ->
-            let h, m = List.assoc "Header" lst, List.assoc "Message" lst in
-            IncRefreshPacket { rp_header = read_header h; rp_msg  = read_inc m }
+        | `Assoc [ ( "SnapshotMessage" , m ) ] -> SnapshotMessage  (read_snap m )
+        | `Assoc [ ( "RefreshMessage"  , m ) ] -> RefreshMessage   (read_inc  m )
+        | _ -> failwith "Unrecognized message entry."              
+;;
         
+        
+let packet_to_json ( packet : CME_Types.packet ) : Yojson.Basic.json  =
+    `Assoc [
+        ( "SeqNum"   , `Int packet.packet_seq_num );
+        ( "Channel"  , packet.packet_channel |> channel_to_json );
+        ( "Messages" , `List ( packet.packet_messages |> List.map message_to_json ))
+    ] 
+;;
 
-let packets_of_json (json : Kojson.json) : packet list =
-    let open Kojson in 
-    match json with
-        | Assoc [( "packets", List packets)] -> List.map packet_of_json packets
-        | _ -> raise (Failure "A single toplevel \"packets\" key is expected.")
-*)
+
+let packet_of_json (json : Yojson.Basic.json) : CME_Types.packet  =
+    let open Yojson.Basic.Util in
+    let open CME_Types in {
+        packet_seq_num  = json |> member "SeqNum"   |> to_int;
+        packet_channel  = json |> member "Channel"  |> channel_of_json;
+        packet_messages = json |> member "Messages" |> ( function
+            | `List msgs -> List.map message_of_json msgs
+            | _ -> failwith "Failed to parse a packet."
+        )
+    }
+;;
+
+
