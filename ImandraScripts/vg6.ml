f741400dd500ad874066ff169a10a6e5a49d6a62
Kostya <kostya@aestheticintegration.com>
2017-10-11 12:01:23 +0000

    Proven vg6

--- ImandraScripts/vg6.ml
+++ ImandraScripts/vg6.ml
@@ -3,10 +3,31 @@
                 populated from incremental channel *)
 (** **************************************************************************************** *)
 
-verify illiquid_nat_refresh (s) =
- (s.channels.cycle_hist_a.liq = Illiquid
-  && s.channels.cycle_hist_b.liq = Illiquid
-  && s.feed_status = InRecovery)
- ==>
+let both_illiquid (s) =
+    s.channels.cycle_hist_a.liq = Illiquid &&  
+    s.channels.cycle_hist_b.liq = Illiquid 
+;;
+
+theorem[rw] illiquid_attempt_recovery (s) =
+    ( both_illiquid s )
+    ==>
+    ( (attempt_recovery s).feed_status = Normal )
+;;
+
+:disable attempt_recovery
+
+theorem[rw] illiquid_constant(s,m,c) =
+    both_illiquid(s) 
+    ==>
+    both_illiquid(process_msg_recovery(s,m,c))
+;;
+
+:disable process_msg_recovery
+
+theorem illiquid_nat_refresh (s) =
+   ( both_illiquid s &&
+     s.feed_status = InRecovery &&
+     get_next_message s <> None )
+    ==>
    ((one_step s).feed_status = Normal)
 ;;
